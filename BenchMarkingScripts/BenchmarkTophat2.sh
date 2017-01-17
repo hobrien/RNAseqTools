c3f646fcc4ced1ae2b57e668d451d75e262d1414
Heath O'Brien <heath.obrien@gmail.com>
2017-01-17 12:20:54 +0000

    Added script to benchmark the time to run Tophat2
    I’m also interested in peak memory usage, so I’ll need to add that

---
+++ BenchMarkingScripts/BenchmarkTophat2.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+#$ -cwd
+#$ -j y
+#$ -S /bin/bash
+#
+
+export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
+
+BASEDIR=~/Benchmark
+# see http://www.tldp.org/LDP/LG/issue18/bash.html for bash Parameter Substitution
+echo "Starting mapping for $SampleID"
+mkdir /c8000xd3/rnaseq-heath/Mappings/$SampleID
+if [ ! -d $BASEDIR/Tophat2 ]
+then
+    echo "Making Tophat2 folder"
+    mkdir $BASEDIR/Tophat2
+    if [ $? -eq 0 ]
+    then
+        echo "Finished making Tophat2 folder"
+    else
+        echo "Could not create Tophat2 folder"
+        exit 1
+    fi    
+Fi
+    
+if [ ! -f $BASEDIR/Tophat2/accepted_hits.bam  ] || [ ! -f $BASEDIR/Tophat2/unmapped.bam ]   
+then
+    echo "Running Tophat2 mapping"
+    time tophat --keep-fasta-order --library-type fr-secondstrand --num-threads 8 \
+      --transcriptome-index /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Annotation/Genes.gencode/genes.inx \
+      --output-dir $BASEDIR/$SampleID \
+      /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Sequence/Bowtie2Index/genome \
+      $BASEDIR/FastQ/test_R1.fastq.gz $BASEDIR/FastQ/test_R2.fastq.gz > $BASEDIR/Tophat2/time.txt
+    if [ $? -eq 0 ]
+    then
+        echo "Finished Tophat mapping "
+    else
+        echo "Tophat mapping failed"
+        exit 1
+    fi    
+fi
