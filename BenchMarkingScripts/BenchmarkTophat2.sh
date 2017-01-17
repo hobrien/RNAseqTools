c1868ceec1f9e642bf161cc325d62cfdae63e7cd
Heath O'Brien <heath.obrien@gmail.com>
2017-01-17 15:50:10 +0000

    added htseq-count, RAM measurement

--- BenchMarkingScripts/BenchmarkTophat2.sh
+++ BenchMarkingScripts/BenchmarkTophat2.sh
@@ -22,14 +22,14 @@ then
     fi    
 fi
     
-if [ ! -f $BASEDIR/Tophat2/accepted_hits.bam  ] || [ ! -f $BASEDIR/Tophat2/unmapped.bam ]   
+if [ ! -f ~/RNAseqTools/BenchMarks/Tophat2_time.txt ]   
 then
     echo "Running Tophat2 mapping"
-    time tophat --keep-fasta-order --library-type fr-secondstrand --num-threads 8 \
+    /usr/bin/time -v -o ~/RNAseqTools/BenchMarks/Tophat2_time.txt tophat --keep-fasta-order --library-type fr-secondstrand --num-threads 8 \
       --transcriptome-index /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Annotation/Genes.gencode/genes.inx \
       --output-dir $BASEDIR/$SampleID \
       /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Sequence/Bowtie2Index/genome \
-      $BASEDIR/FastQ/test_R1.fastq.gz $BASEDIR/FastQ/test_R2.fastq.gz > $BASEDIR/Tophat2/time.txt
+      $BASEDIR/FastQ/test_R1.fastq.gz $BASEDIR/FastQ/test_R2.fastq.gz 
     if [ $? -eq 0 ]
     then
         echo "Finished Tophat mapping "
@@ -38,3 +38,34 @@ then
         exit 1
     fi    
 fi
+
+if [ ! -f $BASEDIR/Tophat2/accepted_hits_nsort.bam ]   
+then
+    echo "Sorting Tophat2 mapping"
+    samtools sort -n $BASEDIR/Tophat2/accepted_hits.bam \
+      $BASEDIR/Tophat2/accepted_hits_nsort 
+    if [ $? -eq 0 ]
+    then
+        echo "Finished sorting Tophat mapping "
+    else
+        echo "Could not sort tophat mapping"
+        exit 1
+    fi    
+fi
+
+if [ ! -f ~/RNAseqTools/BenchMarks/Tophat2_counts.txt ]   
+then
+    echo "Running htseq-count on sorted Tophat2 mapping"
+    htseq-count -f bam -s reverse -t exon -i gene_id -m intersection-strict \
+      $BASEDIR/Tophat2/accepted_hits_nsort.bam \
+      /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Annotation/Genes.gencode/genes.gtf \
+      > ~/RNAseqTools/BenchMarks/Tophat2_counts.txt
+    if [ $? -eq 0 ]
+    then
+        echo "Finished running htseq-count sorted Tophat mapping "
+    else
+        echo "htseq-count could not be run on sorted tophat mapping"
+        exit 1
+    fi    
+fi
+
