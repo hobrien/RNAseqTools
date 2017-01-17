01d9550fa2c1011c090ddd0df58d0f49ee39615d
Heath O'Brien <heath.obrien@gmail.com>
2017-01-17 16:09:54 +0000

    moved name of mapper to a variable
    again, this will make code reuse easier

--- BenchMarkingScripts/BenchmarkTophat2.sh
+++ BenchMarkingScripts/BenchmarkTophat2.sh
@@ -6,49 +6,49 @@
 #
 
 export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
-
-BASEDIR=~/Benchmark/Tophat2
+MAPPER=Tophat2
+BASEDIR=~/Benchmark/$MAPPER
 # see http://www.tldp.org/LDP/LG/issue18/bash.html for bash Parameter Substitution
 if [ ! -d $BASEDIR ]
 then
-    echo "Making Tophat2 folder"
+    echo "Making $MAPPER folder"
     mkdir $BASEDIR
     if [ $? -eq 0 ]
     then
-        echo "Finished making Tophat2 folder"
+        echo "Finished making $MAPPER folder"
     else
-        echo "Could not create Tophat2 folder"
+        echo "Could not create $MAPPER folder"
         exit 1
     fi    
 fi
     
-if [ ! -f ~/RNAseqTools/BenchMarks/Tophat2_time.txt ]   
+if [ ! -f ~/RNAseqTools/BenchMarks/${MAPPER}_time.txt ]   
 then
-    echo "Running Tophat2 mapping"
-    /usr/bin/time -v -o ~/RNAseqTools/BenchMarks/Tophat2_time.txt tophat --keep-fasta-order --library-type fr-secondstrand --num-threads 8 \
+    echo "Running $MAPPER mapping"
+    /usr/bin/time -v -o ~/RNAseqTools/BenchMarks/${MAPPER}_time.txt tophat --keep-fasta-order --library-type fr-secondstrand --num-threads 8 \
       --transcriptome-index /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Annotation/Genes.gencode/genes.inx \
       --output-dir $BASEDIR \
       /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Sequence/Bowtie2Index/genome \
       ~/Benchmark/FastQ/test_R1.fastq.gz ~/Benchmark/FastQ/test_R2.fastq.gz 
     if [ $? -eq 0 ]
     then
-        echo "Finished Tophat mapping "
+        echo "Finished $MAPPER mapping "
     else
-        echo "Tophat mapping failed"
+        echo "$MAPPER mapping failed"
         exit 1
     fi    
 fi
 
-if [ ! -f ~/RNAseqTools/BenchMarks/Tophat2_flagstat.txt ]   
+if [ ! -f ~/RNAseqTools/BenchMarks/${MAPPER}_flagstat.txt ]   
 then
-    echo "Running samtools flagstat Tophat2 mapping"
+    echo "Running samtools flagstat $MAPPER mapping"
     samtools flagstat -n $BASEDIR/accepted_hits.bam \
-      > ~/RNAseqTools/BenchMarks/Tophat2_flagstat.txt
+      > ~/RNAseqTools/BenchMarks/${MAPPER}_flagstat.txt
     if [ $? -eq 0 ]
     then
-        echo "Finished running samtools flagstat on Tophat mapping "
+        echo "Finished running samtools flagstat on $MAPPER mapping "
     else
-        echo "samtools flagstat could not be run on tophat mapping"
+        echo "samtools flagstat could not be run on $MAPPER mapping"
         exit 1
     fi    
 fi
@@ -56,30 +56,30 @@ fi
 
 if [ ! -f $BASEDIR/accepted_hits_nsort.bam ]   
 then
-    echo "Sorting Tophat2 mapping"
+    echo "Sorting $MAPPER mapping"
     samtools sort -n $BASEDIR/accepted_hits.bam \
       $BASEDIR/accepted_hits_nsort 
     if [ $? -eq 0 ]
     then
-        echo "Finished sorting Tophat mapping "
+        echo "Finished sorting $MAPPER mapping "
     else
-        echo "Could not sort tophat mapping"
+        echo "Could not sort $MAPPER mapping"
         exit 1
     fi    
 fi
 
-if [ ! -f ~/RNAseqTools/BenchMarks/Tophat2_counts.txt ]   
+if [ ! -f ~/RNAseqTools/BenchMarks/${MAPPER}_counts.txt ]   
 then
-    echo "Running htseq-count on sorted Tophat2 mapping"
+    echo "Running htseq-count on sorted $MAPPER mapping"
     htseq-count -f bam -s reverse -t exon -i gene_id -m intersection-strict \
       $BASEDIR/accepted_hits_nsort.bam \
       /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Annotation/Genes.gencode/genes.gtf \
-      > ~/RNAseqTools/BenchMarks/Tophat2_counts.txt
+      > ~/RNAseqTools/BenchMarks/${MAPPER}_counts.txt
     if [ $? -eq 0 ]
     then
-        echo "Finished running htseq-count on sorted Tophat mapping "
+        echo "Finished running htseq-count on sorted $MAPPER mapping "
     else
-        echo "htseq-count could not be run on sorted tophat mapping"
+        echo "htseq-count could not be run on sorted $MAPPER mapping"
         exit 1
     fi    
 fi
